d14c0e2958349f56e3afbe2c1425a29bab90052b
Andre Besner <besner.andre@gmail.com>
2023-05-25 17:46:55 -0400

    make tables, create seed file to populate

--- server.js
+++ server.js
@@ -8,3 +8,16 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL Username
+      user: 'root',
+      // TODO: Add MySQL Password
+      password: 'Yaweh25%!',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
