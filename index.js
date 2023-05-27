36ad78e613fc8c1d21f5e8b086097eaa88c6cfd5
Andre Besner <besner.andre@gmail.com>
2023-05-26 20:03:56 -0400

    add switchcase for user function selection for database

--- index.js
+++ index.js
@@ -22,7 +22,35 @@ let init = () => {
     inquirer.prompt(questions)
     .then(data => {
         console.log(data);
+        // take answers and make object to run switch statement
+        const {databaseFunction} = data ;
+        //make switchase to run function correspoding to chosen option 
+        switch(databaseFunction){
+          case 'view all departments':
+            //function to view departments
+            break;
+          case 'view all roles':
+            //function to view roles
+            break;
+          case 'view all employees':
+            //function to view employess
+            break;
+          case 'add a department':
+            //function to add deparment
+            break;
+          case 'add a role':
+            //function to add role
+            break;
+          case 'add an employee':
+            //function to add employee
+            break;
+          case 'update an employee role':
+            //function to update employee role:
+            break;
+        }
+
         init();
+
     })
 }
 
